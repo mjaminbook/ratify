aad53b502cd820d779821ddebbe1176bc122da74
mjaminbook <bmathers@macalester.edu>
2016-12-01 10:44:11 -0600

    Presumably finished views.py and urls.py for backend. AJAX reponses untested. Also templating is not completed.

--- ratify/ratifyApp/models.py
+++ ratify/ratifyApp/models.py
@@ -6,7 +6,13 @@ from django.db import models
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 
+	def __str__(self):
+		return self.question_text
+
 class Answer(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	answer_text = models.CharField(max_length=200)
 	num_votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.answer_text
--- ratify/ratifyApp/views.py
+++ ratify/ratifyApp/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import Answer, Question
 
 # Create your views here.
 def questions(request):
+	questions = Question.objects.all()
 	return HttpResponse("Question Page")
 
-def vote(request, question_id):
-	return HttpResponse("Vote page for question #%s" %question_id)
+def answer(request, question_id):
+	question = Question.objects.get(pk=question_id)
+	answers = Answer.objects.filter(question_id=question_id)
+	return render(request,"answer_page.html")
+
+#The next three functions all expect POST requests
+def vote(request):
+	answerId = request.POST.get("answer_id")
+	answer = Answer.objects.get(pk=answer_id)
+	answer.num_votes += 1
+	answer.save(update_fields=["num_votes"])
+	response = {"status" : 200, "answer_id" : answerId, "total_votes" : answer.num_votes}
+	return HttpResponse(json.dumps(response), content_type="application/json")
+
+def add_answer(request):
+	questionId = request.POST.get("question_id")
+	answerText = request.POST.get("answer_text")
+	question = Question.objects.get(pk=questionId)
+	newAnswer = Answer(question, answerText, 1) #Should we assume someone adding an answer is a vote
+	newAnswer.save()
+
+	response = {"status" : 200, "answer_id" : newAnswer.pk, "answer_text" : answerText}
+	return HttpResponse(json.dumps(response), content_type="application/json")
+
+def add_question(request):
+	questionText = request.POST.get("question_text")
+	newQuestion = Question(questionText)
+	newQuestion.save()
+
+	response = {"status" : 200, "question_id" : newQuestion.pk, "question_text" : questionText}
+	return HttpResponse(json.dumps(response), content_type="application/json")
